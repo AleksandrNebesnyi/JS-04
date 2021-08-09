ced0c4648c3b1794cce856ae452eac0d989db4f3
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-08-09 16:09:26 +0300

    1

--- js/test.js
+++ js/test.js
@@ -1,7 +1,174 @@
-function makePizza() {
-  return "Ваша пицца готовится, ожидайте.";
-}
-// Пиши код ниже этой строки
+// function makePizza() {
+//   return "Ваша пицца готовится, ожидайте.";
+// }
+// // Пиши код ниже этой строки
 
-const result = makePizza();
-const pointer = makePizza;
+// const result = makePizza();
+// const pointer = makePizza;
+
+// console.log(result);
+// console.log(pointer);
+
+// -------------------------------------------
+
+// function deliverPizza(pizzaName) {
+//   return `Доставляем пиццу ${pizzaName}.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Пицца ${pizzaName} готовится, ожидайте...`;
+// }
+
+// // Пиши код ниже этой строки
+// function makeMessage(pizzaName, callback) {
+//   callback(pizzaName);
+
+//   return callback(pizzaName);
+// }
+
+// makeMessage("Ультрасыр", deliverPizza);
+// console.log(makeMessage);
+//  ----------------------------------------------------------
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Роял гранд", function deliverPizza(pizzaName) {
+//   console.log(`Доставляем пиццу ${pizzaName}.`);
+// });
+// // Пиши код ниже этой строки
+
+// makePizza("Ультрасыр", function eatPizza(pizzaName) {
+//   console.log(`Едим пиццу ${pizzaName}`);
+// });
+// ---------------------------------------------------------------------
+// const pizzaPalace = {
+//   pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
+//   order(pizzaName, makePizza, onOrderError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       console.log(this.pizzas.includes(pizzaName));
+//       return makePizza(pizzaName);
+//     }
+//     return onOrderError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+//   },
+// };
+
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// pizzaPalace.order("Аль Копчино", makePizza, onOrderError);
+// console.log(pizzaPalace.order);
+// // console.log(pizzaPalace.order);
+// // pizzaPalace.order("Четыре нарезона", makePizza, onOrderError);
+// // pizzaPalace.order("Биг майк", makePizza, onOrderError);
+// // pizzaPalace.order("Венская", makePizza, onOrderError);
+
+// ---------------------------------------------------------------
+
+// const pizzaPalace = {
+//   pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
+//   // Пиши код ниже этой строки
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+//     }
+
+//     return `Заказ принят, готовим пиццу «${pizzaName}».`;
+//   },
+//   // Пиши код выше этой строки
+// };
+// pizzaPalace.order("Аль Копчино");
+// console.log(pizzaPalace.order("Аль Копчино"));
+
+// ---------------------------------------------------------------------
+
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+//   // Пиши код ниже этой строки
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Пиши код выше этой строки
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+
+// ----------------------------------------------------------------
+
+const orders = [
+  { email: "solomon@topmail.ua", dish: "Burger" },
+  { email: "artemis@coldmail.net", dish: "Pizza" },
+  { email: "jacob@mail.com", dish: "Taco" },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  for (const order of composeMessage.call(orders)) {
+    console.log(composeMessage.call(orders));
+  }
+
+  messsges.push(
+    `Готовим ${this.dish} для ${this.email}. Ваш заказ <позиция>-й в очереди.`
+  );
+}
+
+const messages = [];
+
+composeMessage(2);
+console.log(messages);
+
+// Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+
+// Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. Ваш заказ <позиция>-й в очереди.' Позиция это значение параметра position - позиция элемента в массиве (на единицу больше чем индекс).
+
+// Не объявляй дополнительные параметры функции composeMessage(position).
+// Используй call для вызова функции в контексте одного объекта-заказа.
+// Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
+// Дополни код так, чтобы в переменной messages получился массив сообщений о статусе заказов из массива orders с помощью цикла for.
+
+// Объявлена переменная orders.
+// Значение переменной orders это исходный массив объектов-заказов.
+// Объявлена функция composeMessage(position).
+// У функции composeMessage объявлен один параметр position.
+// В теле функции composeMessage используется this.
+// Функция composeMessage вызывается методом call внутри цикла for.
+// Вызов функции composeMessage с правильными параметами возвращает строку.
+// Объявлена переменная messages.
+// Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
