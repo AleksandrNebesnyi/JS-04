0eabc4b56d5907036cb8ed8392c0e13aed5f8237
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-08-10 12:01:54 +0300

    5

--- js/test.js
+++ js/test.js
@@ -131,44 +131,131 @@
 // console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
 
 // ----------------------------------------------------------------
+// CALL(wrong)
 
-const orders = [
-  { email: "solomon@topmail.ua", dish: "Burger" },
-  { email: "artemis@coldmail.net", dish: "Pizza" },
-  { email: "jacob@mail.com", dish: "Taco" },
-];
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
 
-// Пиши код ниже этой строки
-function composeMessage(position) {
-  for (const order of composeMessage.call(orders)) {
-    console.log(composeMessage.call(orders));
-  }
+// // Пиши код ниже этой строки
 
-  messsges.push(
-    `Готовим ${this.dish} для ${this.email}. Ваш заказ <позиция>-й в очереди.`
-  );
-}
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
 
-const messages = [];
+// const messages = [];
 
-composeMessage(2);
-console.log(messages);
+// for (const order of orders) {
+//   console.log(composeMessage.call(order));
+//   messages.push(composeMessage.call(order));
+// }
 
-// Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+// console.log(messages);
+// composeMessage(2);
 
-// Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. Ваш заказ <позиция>-й в очереди.' Позиция это значение параметра position - позиция элемента в массиве (на единицу больше чем индекс).
+// ---------------------------------------------------------------------------------------
+// CALL
 
-// Не объявляй дополнительные параметры функции composeMessage(position).
-// Используй call для вызова функции в контексте одного объекта-заказа.
-// Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
-// Дополни код так, чтобы в переменной messages получился массив сообщений о статусе заказов из массива orders с помощью цикла for.
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
 
-// Объявлена переменная orders.
-// Значение переменной orders это исходный массив объектов-заказов.
-// Объявлена функция composeMessage(position).
-// У функции composeMessage объявлен один параметр position.
-// В теле функции composeMessage используется this.
-// Функция composeMessage вызывается методом call внутри цикла for.
-// Вызов функции composeMessage с правильными параметами возвращает строку.
-// Объявлена переменная messages.
-// Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
+// // Пиши код ниже этой строки
+// const messages = [];
+
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+
+// for (let i = 0; i < orders.length; i += 1) {
+//   messages.push(composeMessage.call(orders[i], i + 1));
+// }
+
+// // тут мы вызываем функцию в контексте каждого объекта (orders[i]) и передаем позицию + 1
+//
+// -----------------------------------------------------------------------------------------
+// APPLY
+
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
+
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+
+// const messages = [];
+// for (let i = 0; i < orders.length; i++) {
+//   const msg = composeMessage.apply(orders[i], [i + 1]);
+//   messages.push(msg);
+// }
+// --------------------------------------------------------------------------------------
+
+// bind
+
+// const pizzaPalace = {
+//   company: "Pizza Palace",
+// };
+
+// const burgerShack = {
+//   company: "Burger Shack",
+// };
+
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
+// // Пиши код ниже этой строки
+
+// const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+// const pizzaPalaceMessage = pizzaPalaceComposer("Манго");
+// console.log(pizzaPalaceMessage);
+
+// const burgerShackComposer = composeMessage.bind(burgerShack);
+// const burgerShackMessage = burgerShackComposer("Поли");
+
+// ---------------------------------------------------------------------------------------------
+
+// const service = {
+//   mailingList: ["mango@mail.com", "poly@hotmail.de", "ajax@jmail.net"],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter((e) => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
+
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
+
+// const firstInvoke = logAndInvokeAction(
+//   "kiwi@mail.uk",
+//   service.subscribe.bind(service)
+// );
+// console.log(firstInvoke);
+// // Почта kiwi@mail.uk добавлена в рассылку.
+
+// console.log(service.mailingList);
+// /* ['mango@mail.com',
+//     'poly@hotmail.de',
+//     'ajax@jmail.net',
+//     'kiwi@mail.uk']*/
+// const secondInvoke = logAndInvokeAction(
+//   "poly@hotmail.de",
+//   service.unsubscribe.bind(service)
+// );
+// console.log(secondInvoke);
+// // // Почта poly@hotmail.de удалена из рассылки.
+
+// console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
